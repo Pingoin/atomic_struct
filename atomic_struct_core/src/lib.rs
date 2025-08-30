c4a0bf9f73609e2e3afbec7ce9cebccca28d6bd0
Pingoin <pdrente@googlemail.com>
2025-08-30 15:17:10 +0200

    feat: prepare for publish

--- atomic_struct_core/src/lib.rs
+++ atomic_struct_core/src/lib.rs
@@ -24,6 +24,7 @@ where T: Default{
 impl<T> AtomicMember<T> 
 where T: Clone
 {
+
     pub async fn get(&self) -> T {
         let val = self.0.lock().await;
         val.clone()
@@ -34,4 +35,17 @@ where T: Clone
         *val = new_val;
     }
     
+}
+
+#[cfg(feature = "serde")]
+impl<T> serde::Serialize for AtomicMember<T>
+where T: serde::Serialize + Clone
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let val = self.0.blocking_lock();
+        val.serialize(serializer)
+    }
 }
